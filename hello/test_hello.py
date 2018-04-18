d5e5be8eb960c01bbdf1e696469f12ca21d9e3ca
Kai Storbeck <kai@xs4all.nl>
2018-04-18 20:50:06 +0200

    fooo

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -18,5 +18,8 @@ class TestHello(unittest.TestCase):
     def additional_test(self):
         self.assertEqual(1, 1)
 
+    def additional_test(self):
+        self.assertEqual(1, 1)
+
     def failabit(self):
         self.fail("foo!")
